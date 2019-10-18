5ca4e31cc76439608c931d9073ad27224136df55
James <james.philippon@gmail.com>
2019-10-18 11:19:33 -0700

    removed old comments

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-// Load up the discord.js library
 const Discord = require("discord.js");
 
-// This is your client. Some people call it `bot`, some people call it `self`,
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values.
 const config = require("./config.json");
 const sql = require("sqlite");
 const ytdl = require('ytdl-core');
