87393a5b8df38c61b27c083b86e408f66d499311
James <james.philippon@gmail.com>
2019-10-30 11:45:59 -0700

    first test of starboard

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function isOdd(num) { return num % 2;};
 client.on('ready', () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   const realshit = client.emojis.get('487855131996585994');
-  client.channels.get('95702402253983744').send(`${realshit} **SHAQTIVATION COMPLETE** ${realshit}`);
+  // client.channels.get('95702402253983744').send(`${realshit} **SHAQTIVATION COMPLETE** ${realshit}`);
   client.user.setActivity(`with ${client.users.size} balls.`, { type: 'PLAYING' })
 	client.music.start(client, {
   // Set the api key used for YouTube!
@@ -577,6 +577,93 @@ client.on("message", async message => {
     message.channel.send(":warning: IT HAS BEEN [0] DAYS SINCE THE LAST PLEX REFERENCE. :warning:");
     return;
   }
+	
+	
+async run(reaction, user) {
+    const message = reaction.message;
+    if (reaction.emoji.name !== '⭐') return;
+    if (message.author.id === user.id) return message.channel.send(`${user}, you cannot star your own messages.`);
+    if (message.author.bot) return message.channel.send(`${user}, you cannot star bot messages.`);
+    const { starboardChannel } = this.client.settings.get(message.guild.id);
+    const starChannel = message.guild.channels.find(channel => channel.name === starboardChannel)
+    if (!starChannel) return message.channel.send(`It appears that you do not have a \`${starboardChannel}\` channel.`); 
+    const fetchedMessages = await starChannel.fetchMessages({ limit: 100 });
+    const stars = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(message.id));
+    if (stars) {
+      const star = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
+      const foundStar = stars.embeds[0];
+      const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
+      const embed = new RichEmbed()
+        .setColor(foundStar.color)
+        .setDescription(foundStar.description)
+        .setAuthor(message.author.tag, message.author.displayAvatarURL)
+        .setTimestamp()
+        .setFooter(`⭐ ${parseInt(star[1])+1} | ${message.id}`)
+        .setImage(image);
+      const starMsg = await starChannel.fetchMessage(stars.id);
+      await starMsg.edit({ embed });
+    }
+    if (!stars) {
+      const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
+      if (image === '' && message.cleanContent.length < 1) return message.channel.send(`${user}, you cannot star an empty message.`);
+      const embed = new RichEmbed()
+        .setColor(15844367)
+        .setDescription(message.cleanContent)
+        .setAuthor(message.author.tag, message.author.displayAvatarURL)
+        .setTimestamp(new Date())
+        .setFooter(`⭐ 1 | ${message.id}`)
+        .setImage(image);
+      await starChannel.send({ embed });
+    }
+  }
+
+  // Here we add the this.extension function to check if there's anything attached to the message.
+  extension(reaction, attachment) {
+    const imageLink = attachment.split('.');
+    const typeOfImage = imageLink[imageLink.length - 1];
+    const image = /(jpg|jpeg|png|gif)/gi.test(typeOfImage);
+    if (!image) return '';
+    return attachment;
+  }
+};	
+	
+  async run(reaction, user) {
+    const message = reaction.message;
+    if (message.author.id === user.id) return;
+    if (reaction.emoji.name !== '⭐') return;
+    const { starboardChannel } = this.client.settings.get(message.guild.id);
+    const starChannel = message.guild.channels.find(channel => channel.name == starboardChannel)
+    if (!starChannel) return message.channel.send(`It appears that you do not have a \`${starboardChannel}\` channel.`); 
+    const fetchedMessages = await starChannel.fetchMessages({ limit: 100 });
+    const stars = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(reaction.message.id));
+    if (stars) {
+      const star = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
+      const foundStar = stars.embeds[0];
+      const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
+      const embed = new RichEmbed()
+        .setColor(foundStar.color)
+        .setDescription(foundStar.description)
+        .setAuthor(message.author.tag, message.author.displayAvatarURL)
+        .setTimestamp()
+        .setFooter(`⭐ ${parseInt(star[1])-1} | ${message.id}`)
+        .setImage(image);
+      const starMsg = await starChannel.fetchMessage(stars.id);
+      await starMsg.edit({ embed });
+      if(parseInt(star[1]) - 1 == 0) return starMsg.delete(1000);
+    }
+  }
+
+  // Now, it may seem weird that we use this in the messageReactionRemove event, but we still need to check if there's an image so that we can set it, if necessary.
+  extension(reaction, attachment) {
+    const imageLink = attachment.split('.');
+    const typeOfImage = imageLink[imageLink.length - 1];
+    const image = /(jpg|jpeg|png|gif)/gi.test(typeOfImage);
+    if (!image) return '';
+    return attachment;
+  };
+};
+	
+	
 
  
   if (command === "howdy") {
