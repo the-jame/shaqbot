00d0474c41a147e50d32f77aabd62cdc11ed09f7
James <james.philippon@gmail.com>
2019-11-06 18:55:39 -0800

    bs empty string fixed

--- app.js
+++ app.js
@@ -243,7 +243,7 @@ client.on("message", async message => {
         "six of one, half dozen of the other", "an acre", "US Men's 11", "UK Women's 7 & 1/2", "a lima bean", "gamer sized :video_game:", "amassed", "in South Carolina until 10/12",
         "husky", "big and tall", "fine", "sizeable", "beach ball", ":chart_with_upwards_trend:", ":chart_with_downwards_trend:", ":bar_chart:", "boomer-sized", "minute", "old-fashioned", "frail", "nutty", "broad", "crooked", "obtuse", "portable", "stubby", "insufficient", "plump", "corn-fed", "flabby", "opulent", "liberal", "confusing", "questionable", "lacking self-confidence", "uncertain", "meager", "lacking quality"];
       var rand = Math.floor((Math.random() * (ballsizes.length - 1)));
-      if(args[0].toLowerCase() === "my") {args[0] = "Your";}
+      if(typeof args[0] === 'undefined' || args[0].toLowerCase() === "my") { message.channel.send("Your ball size is " + ballsizes[rand] +".", {tts: true});return;}
       else if(args[0].toLowerCase() === "your" || args[0].toLowerCase() === "shaq's" || args[0].toLowerCase() === "shaqs") {args[0] = "My";}
       else if(args[0].toLowerCase() === "shaq" && typeof args[1] === 'undefined') {message.channel.send("My ball size is " + ballsizes[rand] + ".", {tts: true});return;}
       const beingSized = args.join(" "); message.channel.send(beingSized + "'s ball size is " + ballsizes[rand] + ".", {tts: true});
