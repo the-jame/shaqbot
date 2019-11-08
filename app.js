469027dab0caa667ad0a75eb877395ea999bb19c
James <james.philippon@gmail.com>
2019-11-08 10:41:19 -0800

    neck length fix

--- app.js
+++ app.js
@@ -340,7 +340,7 @@ client.on("message", async message => {
       let neck = `${wut2}`;
       let wholeNeck = '';
 
-      let neckLen = Math.floor((Math.random() * (20)));
+      let neckLen = Math.floor((Math.random() * (35)));
       for (i=0; i<neckLen; i++)
       {
         wholeNeck += neck;
