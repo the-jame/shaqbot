9d0014317df5102214673edc4399328ae0e0b07c
James Philippon <james.philippon@gmail.com>
2019-11-20 03:25:37 +0000

    shaq spoken again

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ client.on("message", async message => {
     // MEME HELP
     // memes
     case 'memes':
-      message.channel.send("> Meme responses:\n`Corn`!\n`breasts` - King of Breasts\n`isthatshaq` - Is that Shaq?\n`beans` - Who invented beans??\n`blacked` - on Xmas day?\n`burrito[mug]` - Put Your Burrito In A Mug\n`yallmindifi` - praise the lord?\n`quean` - BEAN QUEAN\n`thinkin` - About Thos Beans\n`consequences` - There. Will. Be. CONSEQUENCES!\n`killed` - This action will kill you immediately.\n`boomer` - OK BOOMER\n`stfuboomer` - STFU boomer.\n`joker` - Dance\n`smoljoker` - Smol joker.\n`doubt` - Doubt\n`chicken` - $5 Rotisserie Chicken Albertson's\n`pungent` - BRRRAPPP\n`uwu` - UwU\n`boomeralert` - Boomer alert!\n`brains` - more than 1% of our brains\n`shaqspoken` - Shaq has spoken.");
+      message.channel.send("> Meme responses:\n`Corn`!\n`breasts` - King of Breasts\n`isthatshaq` - Is that Shaq?\n`beans` - Who invented beans??\n`blacked` - on Xmas day?\n`burrito[mug]` - Put Your Burrito In A Mug\n`yallmindifi` - praise the lord?\n`quean` - BEAN QUEAN\n`thinkin` - About Thos Beans\n`consequences` - There. Will. Be. CONSEQUENCES!\n`killed` - This action will kill you immediately.\n`boomer` - OK BOOMER\n`stfuboomer` - STFU boomer.\n`joker` - Dance\n`smoljoker` - Smol joker.\n`doubt` - Doubt\n`chicken` - $5 Rotisserie Chicken Albertson's\n`pungent` - BRRRAPPP\n`uwu` - UwU\n`boomeralert` - Boomer alert!\n`brains` - more than 1% of our brains\n`spoken` - Shaq has spoken\n`shaqspoken` - \'Shaq\' has spoken.");
       break;
 
     // MEME TEXT
@@ -181,6 +181,10 @@ client.on("message", async message => {
     case 'shaqspoken':
       message.delete().catch(O_o=>{});
       message.channel.send({files: ["img/shaqspoken.png"]});
+      break;
+    case 'spoken':
+      message.delete().catch(O_o=>{});
+      message.channel.send({files: ["img/spoken.jpg"]});
       break;
     case 'isthatshaq':
       message.channel.send({files: ["img/isthatshaq.png"]});
