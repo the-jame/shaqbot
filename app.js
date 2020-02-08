06843d1538978c2612c31a625cb948397a96532b
James Philippon <james.philippon@gmail.com>
2020-02-08 00:43:32 +0000

    idle boolin

--- app.js
+++ app.js
@@ -90,12 +90,12 @@ client.on("message", async message => {
   // and not get into a spam loop (we call that "botception").
   if(message.author.bot) return;
 
-//  if(message.author.presence.status == "idle"){
-//    let user = message.author.id;
-//    //message.delete().catch(O_o=>{});
-//    message.channel.send("Aren't you idle <@" + user + ">? :thinking:");
-//    return;
-//   }
+if(message.author.presence.status == "idle" && idle == 1){
+    let user = message.author.id;
+    //message.delete().catch(O_o=>{});
+    message.channel.send("Aren't you idle <@" + user + ">? :thinking:");
+    return;
+   }
 
   if(message.content.toLowerCase() == 'lol' && lol == 1){
 	setTimeout(()=> {
