2c4c12e8fb18ba8eba80165579d7a7b8e8d9723d
James <jlphilippon@me.com>
2024-04-26 13:56:14 -0700

    lowered mock chance

--- main.js
+++ main.js
@@ -324,7 +324,7 @@ client.on("messageCreate", async message => {
      message.channel.send({content: botDMResponse, flags: 12});
   }
 
-  if(Math.random() < 0.004) {
+  if(Math.random() < 0.0015) {
     command = "mock";  args = message.content.trim().split(/ +/g);
   }
   else if(message.content.indexOf(pref) == 0){
